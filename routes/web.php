bd58b628545184e8d22ed5427f062a5b9916c0fd
fhermas22 <franciscohermas@gmail.com>
2025-01-30 09:58:04 +0100

    Initial commit

---
+++ routes/web.php
@@ -0,0 +1,15 @@
+<?php
+
+use Illuminate\Support\Facades\Route;
+
+Route::get('/', function () {
+    return view('welcome');
+});
+
+Route::get('/second', function() {
+    return view('second');
+});
+
+Route::get('/home', function() {
+    return view('home');
+});
